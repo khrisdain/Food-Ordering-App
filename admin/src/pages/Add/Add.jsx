3f0afa4b4ca1e49196ecea336725d2a13566dcfd
Olamide <samuelilesanmi15@gmail.com>
2024-10-06 22:58:08 +0100

    Some changes

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import "./Add.css";
+import { assets } from '../../assets/assets';
 
 const Add = () => {
   return (
-    <div>Add</div>
+    <div className='add'>
+      <form action="flex-col">
+        <div className='add-img-upload flex-col'>
+          <p>Upload Image</p>
+          <label htmlFor="image">
+            <img src={assets.upload_area} alt="" />
+          </label>
+          <input type="file" id="image" hidden required />
+        </div>
+
+        <div className="add-product-name flex-col"></div>
+      </form>
+    </div>
   )
 }
 
-export default Add
-//Resting
+export default Add;
