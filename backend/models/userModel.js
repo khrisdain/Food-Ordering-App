2802a3023164cc13708bbeaca084726ef8f64eaf
Olamide <samuelilesanmi15@gmail.com>
2024-10-25 20:22:01 +0100

    added cart object

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  name: {type: String, requrired: true},
+  name: {type: String, required: true},
   email: {type: String, required: true},
   password: {type: String, required:true},
   cartData: {type: Object, default: {}}
