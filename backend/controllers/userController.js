0585b8fc9c0f205cfd7a01f6b37f26ff5e52ae90
Olamide <samuelilesanmi15@gmail.com>
2024-10-26 23:25:34 +0100

    updated function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,75 +4,67 @@ import bcrypt from "bcrypt";
 import validator from "validator"
 import 'dotenv/config'
 
-
-const registerUser = async(req, res) => {
-    const {name, password, email} = req.body
-    try{
-        //check if user exists 
-        const exists = await userModel.findOne({email})
-        if(exists){
-            return res.json({success:false, message:"user already exists"})
-        }
-        else{
-
-        }
-
-        //Validating email format & strong password
-        if(!validator.isEmail(email)){
-            res.json({success: false, message:"pls enter a valid email"})
-        }
-
-        if(password.length < 8){
-            return res.json({success: false, message:"Pls enter a strong password"})
-        }
-
-        //hasing user password
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password, salt)
-
-        //pull from req.bodyhttp
-        const newUser = new userModel({
-            name: name,
-            email: email,
-            password: hashedPassword
-        });
-
-        const user = await newUser.save()
-        const token = createToken(user._id)
-        res.json({success: true, token})
-    }catch (error){
-        console.log(error)
-        res.json({success: false, message: "Error"})
-    }
-}
-//login User
-const loginUser = async(req, res) => {
-    const {email, password} = req.body;
-    try {
-        const user = await userModel.findOne({email});
-
-        if(!user){
-            return res.json({success: false, message:"User doesn't exist"})
-        }
-
-        const isMatch = await bcrypt.compare(password, user.password);
-        if(!isMatch){
-            return res.json({success:false, message:"invalid credential"})
-        }
-        const token = createToken(user._id)
-        return res.json({success:true, token})
-
-    }catch(error){
-        console.log(error)
-        return res.json({success:false, message:"Error"})
-    }
-}
-
-
-//jwt Token
+//jwt generation
 const createToken = (id) => {
-    return jwt.sign({id}, process.env.JWT_SECRET) //args: Payload and Signature
-}
+    return jwt.sign({ id }, process.env.JWT_SECRET, {expiresIn: '1d'})
+};
 
+
+//login user
+const loginUser = async (req, res) => {
+    const {email, password} = req.body
+
+    try{
+        const user = await userModel.findOne({ email })
+        //check if user exists
+        if(!user) return res.status(400).json({success: false, message: "User doesn't exists"})
+
+        //Match user password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) return res.status(400).json({success: false, message:"Invalid Credential"})
+        
+        const token = createToken(user._id);
+        res.status(200).json({success:true, token})
+    }
+    catch(error){
+        console.log(error);
+        res.status(500).json({success: false, message: "Internal server error"})
+    }
+};
+
+
+
+//Register a User
+const registerUser = async (req, res) => {
+    const {name, email, password} = req.body;
+    console.log(req.body)
+    try{
+        //check if user already exists
+        const exists = await userModel.findOne({ email })
+        if(exists) {return res.status(400).json({success: false, message: "User already exists"})}
+
+        //validate email usef in registeration
+        if(!validator.isEmail(email)) {return res.status(400).json({success: false, message: "Invalid email"})}
+        
+        //Validate password length
+        if(password.length < 8) {return res.status(400).json({success: false, message: "password is too weak "})}
+
+        //Hash password
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
+
+        const newUser = new userModel({ name, email, password: hashedPassword}); //create user details wrt ajusted args
+        const user = await newUser.save();
+
+        
+        const token = createToken(user._id);
+        res.status(201).json({success: true, token});
+        
+    } catch(error){
+        console.error(error)
+        res.status(500).json({success: false, message:"Internal server error"})
+    }
+}
 
 export {loginUser, registerUser}
+
