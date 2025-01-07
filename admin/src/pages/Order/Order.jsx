a87bc20bcab89028e372650ad26ce65bc574eece
Olamide <samuelilesanmi15@gmail.com>
2025-01-07 04:14:58 +0100

    Admin Order Tab

--- admin/src/pages/Order/Order.jsx
+++ admin/src/pages/Order/Order.jsx
@@ -43,16 +43,14 @@ const Order = ({url}) => {
                 })}
               </p>
               <p className='order-item-name'> {order.address.firstName + " " + order.address.lastName} </p>
-            </div>
 
             <div className="order-item-address">
               <p>{order.address.street+","}</p>
               <p>{order.address.city+", "+order.address.state+", "+order.address.country+", "+order.address.zipcode}</p>
             </div>
-
-            <div>
-              <p className='order-item-phone'>{order.address.phone}</p>
+                  <p className='order-item-phone'>{order.address.phone}</p>
             </div>
+
 
             <p>Items: {order.items.length}</p>
             <p>${order.amount}</p>
