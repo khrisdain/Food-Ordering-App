5391406c9c303150d4bd896f8333667dd1ffd2e3
Olamide <samuelilesanmi15@gmail.com>
2025-01-08 20:45:49 +0100

    Order status update for admin tab

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import authMiddleware from "../middleware/auth.js";
-import  { placeOrder, verifyOrder, userOrders, listOrders, updateFoodStatus } from "../controllers/orderController.js";
+import  { placeOrder, verifyOrder, userOrders, listOrders, updateStatus } from "../controllers/orderController.js";
 
 
 const orderRouter = express.Router();
@@ -8,7 +8,7 @@ const orderRouter = express.Router();
 orderRouter.post("/place", authMiddleware, placeOrder)
 orderRouter.post("/verify", verifyOrder)
 orderRouter.post("/userOrders", authMiddleware, userOrders)
-orderRouter.post("/updateFood", updateFoodStatus)
+orderRouter.post("/status", updateStatus)
 orderRouter.get("/list", listOrders)
 
-export default orderRouter; 
+export default orderRouter;  
