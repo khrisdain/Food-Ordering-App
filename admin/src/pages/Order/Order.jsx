f0c419c8291c2db964889406aee073426ca62452
Olamide <samuelilesanmi15@gmail.com>
2025-01-26 21:54:14 -0500

    Final Commit

--- admin/src/pages/Order/Order.jsx
+++ admin/src/pages/Order/Order.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
 import React, {useState, useEffect} from 'react'
 import './Order.css'
 import { toast } from 'react-toastify'
@@ -32,7 +34,7 @@ const Order = ({url}) => {
 
     useEffect(() => {
         fetchAllOrders();
-    }, [])
+    }, []) //declared empty dependency array
 
 
   return (
